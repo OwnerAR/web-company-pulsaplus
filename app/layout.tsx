252c71ae37acb76c4ad9fd963f732a3e04e4dfc4
a <a@adeMacBook-Pro.local>
2025-10-21 20:37:07 +0700

    fix: Move Google Tag Manager script to correct location in head section
    - Move GTM script from body to head section as required by Google
    - Keep noscript fallback in body section
    - This should resolve GTM verification failure in Google Search Console
    - GTM script now loads before page content for proper tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,21 @@ export default function RootLayout({
   return (
     <html lang="id">
       <head>
+        {/* Google Tag Manager */}
+        {gtmId && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${gtmId}');
+              `,
+            }}
+          />
+        )}
+        
         {/* Schema.org Structured Data */}
         <script
           type="application/ld+json"
@@ -85,6 +100,18 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        {gtmId && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        )}
+        
         {/* Google Analytics */}
         {gaId && (
           <>
@@ -103,29 +130,6 @@ export default function RootLayout({
                 });
               `}
             </Script>
-          </>
-        )}
-        
-        {/* Google Tag Manager */}
-        {gtmId && (
-          <>
-            <Script id="google-tag-manager" strategy="afterInteractive">
-              {`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `}
-            </Script>
-            <noscript>
-              <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
-                height="0"
-                width="0"
-                style={{ display: 'none', visibility: 'hidden' }}
-              />
-            </noscript>
           </>
         )}
         
